82e4a456e5e8a3ffb61bbe7c6d77df7cf15d0ee5
Tobias Krais <tk@design-to-use.de>
2026-04-29 23:54:46 +0200

    Füge CSRF-Schutz für Modul-Installation, -Update und -Deinstallation hinzu und verbessere die Fehlerbehandlung im Setup.

--- pages/help.changelog.php
+++ pages/help.changelog.php
@@ -6,7 +6,7 @@
 <p>1.1.1-DEV:</p>
 <ul>
 	<li>Backend: CSRF-Schutz fuer Speichern-, Loesch- und Statusaktionen der Historienverwaltung ergaenzt.</li>
-	<li>...</li>
+	<li>Backend: CSRF-Schutz fuer Modul-Installation, -Update und -Deinstallation auf der Setup-Seite ergaenzt.</li>
 </ul>
 <p>1.1.0:</p>
 <ul>
